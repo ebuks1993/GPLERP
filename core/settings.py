4c7c4eb88329f3be7a9bc1f3802238fb20b30915
Chukwuka Ebuka <chukwuka932@gmail.com>
2023-11-19 15:05:20 +0100

    first commit

--- core/settings.py
+++ core/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     # 'querycount.middleware.QueryCountMiddleware'  
     
 ]
